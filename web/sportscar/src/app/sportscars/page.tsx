4f50463f8f8249319e653711f747e6918ffc22fe
kota-jiro <joshua.playda.7@gmail.com>
2024-11-26 21:06:33 +0800

    Making Car File Page

--- web/sportscar/src/app/sportscars/page.tsx
+++ web/sportscar/src/app/sportscars/page.tsx
@@ -1,5 +1,24 @@
+"use client";
+import React, { useState } from "react"
+import { motion } from 'framer-motion'
+import Link from "next/link";
+import Image from "next/image";
 
-import React from "react"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
+import { BsArrowUpRight, BsGithub, BsCar, BsCarFrontFill, BsCarFront } from 'react-icons/bs'
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+const sportscar = [
+  {
+    num: '01',
+    brand: 'Nissan',
+    model: 'Skyline R34 GT-R',
+    description: '',
+  }
+]
 
 const SportsCars = () => {
   return (
