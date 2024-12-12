67f798ba3632de3d62442275576b23dad24a2875
kota-jiro <joshua.playda.7@gmail.com>
2024-12-12 12:21:54 +0800

    Ticket: P5Cdiyo7

--- backend/sports-cars/app/Domain/Admin/Admin.php
+++ backend/sports-cars/app/Domain/Admin/Admin.php
@@ -4,70 +4,94 @@ namespace App\Domain\Admin;
 
 class Admin
 {
-    private $id;
-    private $name;
-    private $email;
-    private $password;
-    private $api_token;
-    private $created_at;
-    private $updated_at;
+    private ?int $id;
+    private ?string $userId;
+    private ?string $firstName;
+    private ?string $lastName;
+    private ?string $username;
+    private ?string $password;
+    private ?string $api_token;
+    private ?string $created_at;
+    private ?string $updated_at;
+    private ?int $roleId;
 
     public function __construct(
         ?int $id = null,
-        string $name = null,
-        string $email = null,
-        string $password = null,
-        string $api_token = null,
-        string $created_at = null,
-        string $updated_at = null
+        ?string $userId = null,
+        ?string $firstName = null,
+        ?string $lastName = null,
+        ?string $username = null,
+        ?string $password = null,
+        ?string $api_token = null,
+        ?string $created_at = null,
+        ?string $updated_at = null,
+        ?int $roleId = null
     )
     {
         $this->id = $id;
-        $this->name = $name;
-        $this->email = $email;
+        $this->userId = $userId;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->username = $username;
         $this->password = $password;
         $this->api_token = $api_token;
         $this->created_at = $created_at;
         $this->updated_at = $updated_at;
+        $this->roleId = $roleId;
     }
     public function toArray(): array
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
+            'userId' => $this->userId,
+            'firstName' => $this->firstName,
+            'lastName' => $this->lastName,
+            'username' => $this->username,
             'password' => $this->password,
             'api_token' => $this->api_token,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'roleId' => $this->roleId,
         ];
     }
     public function getId(): ?int
     {
         return $this->id;
     }
-    public function getName(): string
+    public function getUserId(): ?string
     {
-        return $this->name;
+        return $this->userId;
     }
-    public function getEmail(): string
+    public function getFirstName(): ?string
     {
-        return $this->email;
+        return $this->firstName;
     }
-    public function getPassword(): string
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+    public function getPassword(): ?string
     {
         return $this->password;
     }
-    public function getApiToken(): string
+    public function getApiToken(): ?string
     {
         return $this->api_token;
     }
-    public function getCreatedAt(): string
+    public function getCreatedAt(): ?string
     {
         return $this->created_at;
     }
-    public function getUpdatedAt(): string
+    public function getUpdatedAt(): ?string
     {
         return $this->updated_at;
+    }
+    public function getRoleId(): ?int
+    {
+        return $this->roleId;
     }
 }
--- backend/sports-cars/app/Domain/Admin/AdminRepository.php
+++ backend/sports-cars/app/Domain/Admin/AdminRepository.php
@@ -6,13 +6,14 @@ interface AdminRepository
 {
     public function findAll(): array;
     public function findById(int $id): ?Admin;
-    public function findByEmail(string $email): ?Admin;
+    public function findByUsername(string $username): ?Admin;
     public function register(Admin $admin): Admin;
     public function update(Admin $admin): Admin;
     public function delete(int $id): void;
-    public function login(string $email, string $password): ?Admin;
+    public function login(string $username, string $password): ?Admin;
     public function addApiToken(int $id, string $api_token): void;
     public function updateApiToken(int $id, string $api_token): void;
     public function findByApiToken(string $api_token): ?Admin;
     public function findByPassword(string $password): ?Admin;
+    public function findByUserId(string $userId): ?Admin;
 }
