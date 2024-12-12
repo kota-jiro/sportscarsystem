33e71e3bdae11290f30e2a7fe03edc3b509325d8
kota-jiro <joshua.playda.7@gmail.com>
2024-12-12 12:21:10 +0800

    Ticket: wkSsugpo

--- backend/sports-cars/resources/views/admin/adminregister.blade.php
+++ backend/sports-cars/resources/views/admin/adminregister.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <h1>Admin Login</h1>
+    <h1>Admin Register</h1>
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -22,8 +22,16 @@
         </div>
     @endif
 
-    <form action="{{ route('admin.login') }}" method="POST">
+    <form action="{{ route('admin.register.submit') }}" method="POST" enctype="multipart/form-data">
         @csrf
+        <div class="form-group">
+            <label for="firstName">First Name:</label>
+            <input type="text" id="firstName" name="firstName" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" class="form-control" required>
+        </div>
         <div class="form-group">
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" class="form-control" required>
@@ -32,7 +40,11 @@
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" class="form-control" required>
         </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-        <button type="button" onclick="window.location.href='{{ route('admin.register') }}'" class="btn btn-secondary mt-3">Register</button>
+        <div class="form-group">
+            <label for="confirmPassword">Confirm Password:</label>
+            <input type="password" id="confirmPassword" name="confirmPassword" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Register</button>
+        <a href="{{ route('admin.login') }}" class="btn btn-secondary">Cancel</a>
     </form>
 </div>
